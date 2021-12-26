b0a51c241f2b7784a7835074976bf4733ce608f1
David Preda <dtpreda30@gmail.com>
2021-12-26 13:00:05 +0000

    Add frame header

---
+++ dll/frame/frame.h
@@ -0,0 +1,20 @@
+#ifndef FRAME
+#define FRAME
+
+// Address Field bit sequences
+#define A_CSAR (unsigned char) '\x03'
+#define A_CRAS (unsigned char) '\x01'
+
+// Control Field bit sequences
+#define SET (unsigned char) '\x03'
+#define DISC (unsigned char) '\x0B'
+#define UA (unsigned char) '\x07'
+#define SEND_I(n) (unsigned char) ((n << 6) | '\x00')
+#define RR(n) (unsigned char) ((n << 7) | '\x05')
+#define REJ(n) (unsigned char) ((n << 7) | '\x01')
+
+int build_su_frame(unsigned char* frame, unsigned char address, unsigned char control, unsigned int is_s);
+
+int build_i_frame(unsigned char *frame, unsigned char address, unsigned char control, unsigned char *data, unsigned int data_size);
+
+#endif FRAME
