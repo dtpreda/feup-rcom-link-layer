1772fca61b7b1f0a072481d9801c623350496809
David Preda <dtpreda30@gmail.com>
2021-12-26 23:32:26 +0000

    Add packet files

---
+++ app/packet/packet.h
@@ -0,0 +1,22 @@
+#ifndef PACKET
+#define PACKET
+
+#define DATA (unsigned char) '\x01'
+
+#define START (unsigned char) '\x02'
+#define END (unsigned char) '\x03'
+
+#define DATA_HEADER_SIZE (unsigned int) 4
+#define CONTROL_HEADER_SIZE(n) (unsigned int) n*2 + 1
+#define MAX_PACKET_SIZE (unsigned int) 512
+#define MAX_DATA_SIZE (unsigned int) MAX_PACKET_SIZE - DATA_HEADER_SIZE
+
+unsigned int build_control_packet(unsigned char *packet, unsigned char control, unsigned char* file_name, unsigned int file_size);
+
+unsigned int process_control_packet(unsigned char *packet, unsigned int size);
+
+unsigned int build_data_packet(unsigned char *packet, unsigned char *data, unsigned int *data_size);
+
+unsigned int process_data_packet(unsigned char *packet, unsigned int size, unsigned char *data, unsigned int *data_size);
+
+#endif /* PACKET */
